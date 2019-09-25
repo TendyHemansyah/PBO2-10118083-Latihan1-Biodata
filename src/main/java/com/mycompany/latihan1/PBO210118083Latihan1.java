c25787f2c90c92167cc44572d97c1a9db156bd02
Tendy Hermansyah <55569231+TendyHemansyah@users.noreply.github.com>
2019-09-25 13:00:23 +0700

    Add files via upload

---
+++ src/main/java/com/mycompany/latihan1/PBO210118083Latihan1.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.latihan1;
+
+/**
+ *
+ * @author Tendy
+ * NAMA     : Tendy Hermansyah
+ * KELAS    : PBO2
+ * NIM      : 10118083
+ * Deskripsi Program : Biodata
+ */
+public class PBO210118083Latihan1 {
+    public static void main(String[] args){
+        System.out.println("BIODATA ROCKSTAR" + "\n" + "-----------------");
+        System.out.println("NAMA\t\t: Tendy Hermansyah");
+        System.out.println("ALAMAT\t\t: Jl. Sekeloa Tengah");
+        System.out.println("TEMPAT LAHIR\t: Sumedang");
+        System.out.println("TANGGAL LAHIR\t: 04 Maret 2000");
+        System.out.println("-----------------");
+        System.out.println("HOBI\t\t: Dengarkan Musik");
+        System.out.println("MAKES\t\t: Nasi Goreng");
+        System.out.println("MIKES\t\t: Kopi");
+    }
+}
